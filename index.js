945e5cd021885009b817f89eadaff153053d8739
ukyzr <shaozuanzuan@gmail.com>
2020-07-10 17:45:30 +0800

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
   GH_TOKEN: githubToken,
   WAKATIME_API_KEY: wakatimeApiKey
 } = process.env;
+console.log(gisId, githubToken, wakatimeApiKey);
 
 const wakatime = new WakaTimeClient(wakatimeApiKey);
 
